f712954dbccd890a045442d9dde87d386f12ce01
kingstar <admin@baks.dev>
2024-12-15 23:49:39 +0300

    Baks Development

--- Repository/AllMegamarketToken/AllMegamarketTokenRepository.php
+++ Repository/AllMegamarketToken/AllMegamarketTokenRepository.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -107,7 +107,7 @@ final class AllMegamarketTokenRepository implements AllMegamarketTokenInterface
             ->addSelect('users_profile.event as users_profile_event')
             ->leftJoin(
                 'token',
-                UserProfile::TABLE,
+                UserProfile::class,
                 'users_profile',
                 'users_profile.id = token.id'
             );
@@ -118,7 +118,7 @@ final class AllMegamarketTokenRepository implements AllMegamarketTokenInterface
             ->addSelect('users_profile_info.status as users_profile_status')
             ->leftJoin(
                 'token',
-                UserProfileInfo::TABLE,
+                UserProfileInfo::class,
                 'users_profile_info',
                 'users_profile_info.profile = token.id'
             );
@@ -126,7 +126,7 @@ final class AllMegamarketTokenRepository implements AllMegamarketTokenInterface
         // Event
         $qb->leftJoin(
             'users_profile',
-            UserProfileEvent::TABLE,
+            UserProfileEvent::class,
             'users_profile_event',
             'users_profile_event.id = users_profile.event'
         );
@@ -137,7 +137,7 @@ final class AllMegamarketTokenRepository implements AllMegamarketTokenInterface
 
         $qb->leftJoin(
             'users_profile_event',
-            UserProfilePersonal::TABLE,
+            UserProfilePersonal::class,
             'users_profile_personal',
             'users_profile_personal.event = users_profile_event.id'
         );
